d32c0fc3d0f5887455a6d27ba8716bc15bdba6c5
Rjansse1 <rob.janssen@capgemini.com>
2024-02-19 15:40:38 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './main-page/header.js';
+import Movie from './movies/movie.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <Header title="Great App" subtitle="Should make more of these" />    
+        <Movie />
+      </div>
     </div>
   );
 }
---
+++ src/movies/movie.js
@@ -0,0 +1,7 @@
+const Movie = () => (
+    <div className="col">
+        Hi there.
+    </div>
+);
+
+export default Movie;
